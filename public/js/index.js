84fd231f3764525da715c352317f4ca3c24a2258
Vinicius Oliveira <viniciusoliveira.inf@gmail.com>
2022-02-21 17:09:00 -0300

    Projeto Nikel

---
+++ public/js/index.js
@@ -0,0 +1,97 @@
+const myModal = new bootstrap.Modal("#register-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+checkLogged();
+
+//logar sistema
+document.getElementById("login-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-input").value;
+    const password = document.getElementById("password-input").value;
+    const checksession = document.getElementById("session-check").checked;
+
+    const account = getAccount(email);
+
+    if(!account) {
+        alert("Opps! Verifique o usuario ou a senha");
+        return;
+    }
+
+    if(account) {
+        if(account.password !== password) {
+            alert("Opps! Verifique o usuario ou a senha.");
+            return;
+        }
+
+        saveSession(email, checksession);
+
+        window.location.href = "home.html";
+
+    }
+});
+
+//criar cota
+document.getElementById("create-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email-create-input").value;
+    const password = document.getElementById("password-create-input").value;
+
+    if(email.length < 5) {
+        alert("Preencha o campo com um e-mail válido");
+        return;
+    }
+
+    if(password.length < 4) {
+        alert("Preencha a senha com no minimo 4 digitos");
+        return;
+    }
+
+    saveAccount({
+        login: email,
+        password: password,
+        transactions: []
+    });
+
+    myModal.hide();
+
+    alert("Conta criada com sucesso!");
+    
+});
+
+function checkLogged() {
+    if(session) {
+        sessionStorage.setItem("logged", session);
+        logged = session
+    }
+
+    if(logged) {
+        saveSession(logged, session);
+        window.location.href = "home.html";
+    }
+}
+
+function saveAccount(data) {
+    console.log(data);
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
+
+function saveSession(data, saveSession) {
+    if(saveSession) {
+        localStorage.setItem("session", data);
+    }
+
+    sessionStorage.setItem("logged", data);
+}
+
+function getAccount(key) {
+    const account = localStorage.getItem(key);
+
+    if(account) {
+        return JSON.parse(account);
+    }
+
+    return "";
+}
